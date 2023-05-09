4f474d39ade16421f49b663aebf7badc0748ba80
paulosilva1278 <paulo.fernandodasilva@gmail.com>
2023-05-09 16:34:31 -0300

    aula preparatória intencivo spring

---
+++ src/main/java/com/devsuperior/demo/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.devsuperior.demo.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.demo.entities.Department;
+import com.devsuperior.demo.entities.Product;
+import com.devsuperior.demo.repositories.ProductRepository;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+	
+	@Autowired
+	private ProductRepository productRepository;
+
+	@GetMapping
+	public List<Product> getObjects() {
+		
+		List<Product> list = productRepository.findAll();
+		
+		return list;
+	}
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into tb_departament(name) values ( 'Tech' );
+insert into tb_departament(name) values ( 'Pet' );
+
+insert into tb_product(name, price, department_id) values( 'Mackbook Pro', 4000.00, 1 );
+insert into tb_product(name, price, department_id) values( 'PC Gamer', 5000.00, 1 );
+insert into tb_product(name, price, department_id) values( 'Pet House', 300.00, 2 );
+
